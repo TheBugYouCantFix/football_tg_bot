5a95770826a196e017ff06e49e2410cbba3db7e0
TheBugYouCantFix <alex.gorbanev05@gmail.com>
2022-04-17 17:06:17 +0500

    df arguments added to funcs

--- parsing_data.py
+++ parsing_data.py
@@ -10,11 +10,12 @@ class InternationalMatchesParser:
         self.df['id'] = self.df.index
         self.df.insert(0, 'id', self.df.pop('id'))
 
-    def get_country_df(self, country):
-        filtered = (self.df['home_team'].str.lower() == country.lower()) |\
-                   (self.df['away_team'].str.lower() == country.lower())
+    @staticmethod
+    def get_country_df(frame: pd.DataFrame, country):
+        filtered = (frame['home_team'].str.lower() == country.lower()) |\
+                   (frame['away_team'].str.lower() == country.lower())
 
-        return self.df[filtered]
+        return frame[filtered]
 
     def get_match_result(self, match_id):
         home_team_name, away_team_name, \
@@ -28,11 +29,11 @@ class InternationalMatchesParser:
 
         return 'Draw'
 
-    def get_n_of_wins_for_country(self, country, official_only=True):
+    def get_n_of_wins_for_country(self, frame: pd.DataFrame, country, official_only=True):
         # Returns number of wins in official(non-friendly) matches for a passes country
         counter = 0
 
-        country_df = self.get_country_df(country)
+        country_df = self.get_country_df(frame, country)
 
         if official_only:
             official_matches = country_df['tournament'] != 'Friendly'
@@ -47,8 +48,8 @@ class InternationalMatchesParser:
 
         return counter
 
-    def matches_played(self, country, official_only=True):
-        country_df = self.get_country_df(country)
+    def matches_played(self, frame, country, official_only=True):
+        country_df = self.get_country_df(frame, country)
 
         if official_only:
             official_matches = country_df['tournament'] != 'Friendly'
@@ -65,15 +66,12 @@ class InternationalMatchesParser:
 
         return self.df[filtered]
 
-    def get_country_win_rate(self, country):
+    def get_country_win_rate(self, country, after_year=-1):
+        frame = self.after_year(after_year)
 
-        win_rate = self.get_n_of_wins_for_country(country) /\
-                   self.matches_played(country)
+        win_rate = self.get_n_of_wins_for_country(frame, country) /\
+                   self.matches_played(frame, country)
 
         win_rate = round(win_rate * 100, 1)
 
         return win_rate
-
-
-imp = InternationalMatchesParser()
-print(imp.after_year(2018))
