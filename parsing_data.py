b964133121ee6bb64c80e6832a8527a045f3eb3f
TheBugYouCantFix <alex.gorbanev05@gmail.com>
2022-04-18 23:32:38 +0500

    win_rate for all countries added

--- parsing_data.py
+++ parsing_data.py
@@ -1,14 +1,18 @@
 import pandas as pd
+import pycountry as pc
+import matplotlib.pyplot as plt
 
 
 class InternationalMatchesParser:
     PATH = './data/results.csv'
     df = pd.read_csv(PATH)
 
-    # Code to add an "id" column:
     def __init__(self):
+        # Code to add an "id" column:
         self.df['id'] = self.df.index
         self.df.insert(0, 'id', self.df.pop('id'))
+
+        self.win_rate_df = None
 
     @staticmethod
     def get_country_df(frame: pd.DataFrame, country):
@@ -73,12 +77,39 @@ class InternationalMatchesParser:
     def get_country_win_rate(self, country, after_year=-1):
         frame = self.after_year(after_year)
 
-        win_rate = self.get_n_of_wins_for_country(frame, country) /\
-                   self.matches_played(frame, country)
+        wins = self.get_n_of_wins_for_country(frame, country)
+        matches_played = self.matches_played(frame, country)
+
+        if matches_played == 0:
+            return -1
+
+        win_rate = wins / matches_played
 
         win_rate = round(win_rate * 100, 1)
 
         return win_rate
 
+    def fill_countries_win_rate_df(self):
+        data = []
 
-    # TODO: implement graphs of win rate
+        for country in pc.countries:
+            name = country.name
+            rate = self.get_country_win_rate(name)
+
+            if rate == -1 and hasattr(country, 'official_name'):
+                name = country.official_name
+                rate = self.get_country_win_rate(name)
+
+            if rate == -1:
+                continue
+
+            data.append([name, rate])
+
+        self.win_rate_df = pd.DataFrame(data, columns=['country', 'win_rate'])
+        sorted_df = self.win_rate_df.sort_values(by=['win_rate'], ascending=False)
+        self.build_win_rate_graph(sorted_df)
+
+    @staticmethod
+    def build_win_rate_graph(df):
+        df.head().plot(x='country', y='win_rate')
+        plt.show()
